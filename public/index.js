dd0be23081da51543269725377a203a41a0c671d
nimbuslink <93158798+nimbuslink@users.noreply.github.com>
2021-10-26 10:36:32 -0500

    loginSystemProject

---
+++ public/index.js
@@ -0,0 +1,5 @@
+var videoJS = videojs("video");
+videoJS.src({
+  src: "https://marshpoint-marshmedia-usso.streaming.media.azure.net/b7d14de4-eed7-4d03-b1c3-2a22e1567d1c/Star Trek The Next Generation Se.ism/manifest(format=m3u8-aapl)",
+  type: "application/x-mpegURL",
+});
---
+++ public/login.js
@@ -0,0 +1,129 @@
+function toggleSignIn() {
+    if (firebase.auth().currentUser) {
+      firebase.auth().signOut();
+    } else {
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
+      if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+      }
+      if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+      }
+      // Sign in with email and pass.
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode === 'auth/wrong-password') {
+          alert('Wrong password.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        document.getElementById('sign-in').disabled = false;
+      });
+    }
+    document.getElementById('sign-in').disabled = true;
+  }
+
+  /**
+   * Handles the sign up button press.
+   */
+  function handleSignUp() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    // Create user with email and pass.
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+    });
+  }
+
+  /**
+   * Sends an email verification to the user.
+   */
+  function sendEmailVerification() {
+    firebase.auth().currentUser.sendEmailVerification().then(function() {
+      // Email Verification sent!
+      alert('Email Verification Sent!');
+    });
+  }
+
+  function sendPasswordReset() {
+    var email = document.getElementById('email').value;
+    firebase.auth().sendPasswordResetEmail(email).then(function() {
+      // Password Reset Email Sent!
+      alert('Password Reset Email Sent!');
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode == 'auth/invalid-email') {
+        alert(errorMessage);
+      } else if (errorCode == 'auth/user-not-found') {
+        alert(errorMessage);
+      }
+      console.log(error);
+    });
+  }
+
+  /**
+   * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+   *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+   *    out, and that is where we update the UI.
+   */
+  function initApp() {
+    // Listening for auth state changes.
+    firebase.auth().onAuthStateChanged(function(user) {
+      document.getElementById('verify-email').disabled = true;
+      if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        document.getElementById('sign-in-status').textContent = 'Signed in';
+        document.getElementById('sign-in').textContent = 'Sign out';
+        document.getElementById('account-details').textContent = JSON.stringify(user, null, '  ');
+        if (!emailVerified) {
+          document.getElementById('verify-email').disabled = false;
+        }
+      } else {
+        // User is signed out.
+        document.getElementById('sign-in-status').textContent = 'Signed out';
+        document.getElementById('sign-in').textContent = 'Sign in';
+        document.getElementById('account-details').textContent = 'null';
+      }
+      document.getElementById('sign-in').disabled = false;
+    });
+
+    document.getElementById('sign-in').addEventListener('click', toggleSignIn, false);
+    document.getElementById('sign-up').addEventListener('click', handleSignUp, false);
+    document.getElementById('verify-email').addEventListener('click', sendEmailVerification, false);
+    document.getElementById('password-reset').addEventListener('click', sendPasswordReset, false);
+  }
+
+  window.onload = function() {
+    initApp();
+  };
